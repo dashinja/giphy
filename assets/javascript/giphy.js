530070d62a20f8d02ed7bcf4a603f613daf55287
DaShinja <byron@dashinja.com>
2019-01-04 20:02:35 -0500

    feat(display): Click Handler for Images
    This feature allows clicking a gif on the page to toggle its animation/still state
    *To allow full preview of the gif without consuming larger amounts of bandwidth for user
    *Selected gifs will animate
    *Reselection stops the animation

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -25,6 +25,9 @@ $(document).ready(function() {
   const apiKey = 'BaQP9zdxQzj0WoZ5JfWm92wsN34uwwNI';
   let buttonMaker;
 
+  let chosenTopic = $(this).attr('data-name');
+  let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${chosenTopic}&limit=10`;
+
   function buttonRender() {
     $('#insert-giphy').empty();
 
@@ -37,55 +40,92 @@ $(document).ready(function() {
     }
   }
 
-  let show10 = function() {
+  function show10() {
     let chosenTopic = $(this).attr('data-name');
-    let queryURL10 = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${chosenTopic}&limit=10`;
-
+    let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${chosenTopic}&limit=10`;
     $.ajax({
-      url: queryURL10,
+      url: queryURL,
       method: 'GET'
     }).then(function(res) {
       console.log(res);
-      // let body = res.json
-      // console.log(body);
-      // return body
+      // Keeps graphic section from appending, shows only the most recent result check
       $('#insert-giphy').empty();
       for (let i = 0; i < topics.length; i++) {
-        let fixed_height_still_url = res.data[i].images.fixed_height_still.url;
-        let animated_url = res.data[i].images.looping.mp4;
+        // these images
+        let stillImage = res.data[i].images.fixed_height_still.url;
+        let animated_url = res.data[i].images.fixed_height.url; //remove me?
+
+        let dataPlace = res.data[i]; // unecessary?
 
         let imgHolder = $('<img>');
 
-        imgHolder.attr('data-imageType', fixed_height_still_url);
-        // let makeMove = imgHolder.attr("data-imageType", fixed_height_still_url)
+        // imgHolder.attr('data-imageType', stillImage );// remove me?
 
-        imgHolder.attr('src', fixed_height_still_url);
+        imgHolder.attr('src', stillImage);
         imgHolder.attr('data-state', 'still');
-        imgHolder.attr('data-still', fixed_height_still_url);
+        imgHolder.attr('data-still', stillImage);
         imgHolder.attr('data-animate', animated_url);
+        imgHolder.attr('data-item', dataPlace);
+        console.log(dataPlace);
         imgHolder.addClass('giphy-image');
         $('#insert-giphy').prepend(imgHolder);
-        // $('#insert-giphy').prepend(`<img src="${fixed_height_still_url}">`);
-
-
-        
+        // $('#insert-giphy').prepend(`<img src="${stillImage )">`);
       }
-    });
-  };
 
-  function AnimateOrNot () {
-        // To Animate or Not to Animate
-        console.log('I exist already and I got clicked!');
-        let state = $(this).attr('data-state');
-        if (state === 'still') {
-          imgHolder.attr('src', animated_url);
-          $(this).attr('data-state', 'animated');
-        } else if (state === 'animated') {
-          imgHolder.attr('src', fixed_height_still_url);
-          $(this).attr('data-state', 'still');
-        } else {
-          alert('You messed up bad!');
-        }
+      // let imagePieces
+      // // {
+      // //   stillImage: stillImage,
+      // //   animated_url: animated_url
+      // // };
+
+      // return imagePieces = {
+      //   stillImage: stillImage,
+      //   animated_url: animated_url
+      // };
+    });
+  }
+
+  function AnimateOrNot() {
+    // To Animate or Not to Animate
+    console.log('I exist already and I got clicked!');
+    console.log('First This :', this);
+    let state = $(this).attr('data-state');
+    console.log(state);
+    let stillImage = $(this).attr('data-still');
+    let animated_url = $(this).attr('data-animate');
+    console.log('Still Image: ', stillImage);
+    console.log('Animated Image: ', animated_url);
+    if (state === 'still') {
+      $(this).attr('src', animated_url);
+      $(this).attr('data-state', 'animated');
+      console.log("I'm the new source: ", $(this).attr('src'));
+    } else if (state === 'animated') {
+      $(this).attr('src', stillImage);
+      $(this).attr('data-state', 'still');
+      console.log("I'm the new source: ", $(this).attr('src'));
+    } else {
+      alert('You messed up bad!');
+    }
+
+    // $.ajax({
+    //   url: queryURL,
+    //   method: 'GET'
+    // }).then(function() {
+    //   let state = this;
+    //   console.log(state);
+    //   let stillImage = res.data[i].images.fixed_height_still.url;
+    //   let animated_url = res.data[i].images.fixed_height.url;
+    //   console.log(animated_url);
+    //   if (state === 'still') {
+    //     imgHolder.attr('src', animated_url);
+    //     $(this).attr('data-state', 'animated');
+    //   } else if (state === 'animated') {
+    //     imgHolder.attr('src', stillImage);
+    //     $(this).attr('data-state', 'still');
+    //   } else {
+    //     alert('You messed up bad!');
+    //   }
+    // });
   }
   // When specific image is clicked, make animate, or stop animation
   // $('.giphies').on('click', 'giphy-image', function() {
@@ -98,7 +138,7 @@ $(document).ready(function() {
   //     imgHolder.attr('src', animated_url);
   //     $(this).attr('data-state', 'animated');
   //   } else if (state === 'animated') {
-  //     imgHolder.attr('src', fixed_height_still_url);
+  //     imgHolder.attr('src', stillImage );
   //     $(this).attr('data-state', 'still');
   //   } else {
   //     alert('You messed up bad!');
